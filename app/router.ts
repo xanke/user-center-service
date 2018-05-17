783f05c7b7aef234a00ba68277c1e5744cbaad11
xank <xank@qq.com>
2018-05-17 19:40:43 +0800

    1

--- app/router.ts
+++ app/router.ts
@@ -8,7 +8,6 @@ export default (app: Application) => {
   // router.post('/v1/sign-in', controller.account.signIn)
   // router.post('/v1/verify-code/sms-code', controller.verify.smsCode)
   // router.post('/v1/verify-code/email-code', controller.verify.emailCode)
-
   // router.post('/v1/api/auth/user-current', controller.auth.userCurrent)
   // router.post('/v1/api/auth/change-password', controller.auth.changePassword)
   // router.post('/v1/api/auth/change-email', controller.auth.changeEmail)
--- config/config.default.ts
+++ config/config.default.ts
@@ -1,25 +1,37 @@
-import { EggAppConfig, PowerPartial } from 'egg';
+import { EggAppConfig, PowerPartial } from 'egg'
 
 // for config.{env}.ts
-export type DefaultConfig = PowerPartial<EggAppConfig & BizConfig>;
+export type DefaultConfig = PowerPartial<EggAppConfig & BizConfig>
 
 // app special config scheme
 export interface BizConfig {
-  sourceUrl: string;
+  sourceUrl: string
 }
 
 export default (appInfo: EggAppConfig) => {
-  const config = {} as PowerPartial<EggAppConfig> & BizConfig;
+  const config = {} as PowerPartial<EggAppConfig> & BizConfig
 
   // app special config
-  config.sourceUrl = `https://github.com/eggjs/examples/tree/master/${appInfo.name}`;
+  config.sourceUrl = `https://github.com/eggjs/examples/tree/master/${
+    appInfo.name
+  }`
 
   // override config from framework / plugin
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1526548823403_7693';
+  config.keys = appInfo.name + '_1526548823403_7693'
 
   // add your config here
-  config.middleware = [];
+  config.middleware = []
 
-  return config;
-};
+  config.sequelize = {
+    dialect: 'mysql',
+    database: 'user-center',
+    host: 'localhost',
+    port: '3306',
+    username: 'root',
+    password: 'root',
+    timezone: '+08:00'
+  }
+
+  return config
+}
