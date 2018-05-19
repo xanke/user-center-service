882a3e53e3993c6b03f4765c636a9aae6c114d8c
xank <xank@qq.com>
2018-05-19 17:16:43 +0800

    format

--- app/router.ts
+++ app/router.ts
@@ -1,13 +1,13 @@
-import { Application } from 'egg'
+import { Application } from 'egg';
 
 export default (app: Application) => {
-  const { controller, router } = app
+  const { controller, router } = app;
 
-  router.get('/', controller.home.index)
-  router.post('/v1/sign-in', controller.account.signIn)
+  router.get('/', controller.home.index);
+  router.post('/v1/sign-in', controller.account.signIn);
   // router.post('/v1/verify-code/sms-code', controller.verify.smsCode)
   // router.post('/v1/verify-code/email-code', controller.verify.emailCode)
   // router.post('/v1/api/auth/user-current', controller.auth.userCurrent)
   // router.post('/v1/api/auth/change-password', controller.auth.changePassword)
   // router.post('/v1/api/auth/change-email', controller.auth.changeEmail)
-}
+};
