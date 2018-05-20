da95162396e438028080b5953f1c3faec20c6468
xank <xank@qq.com>
2018-05-21 00:26:43 +0800

    user create

--- app/controller/account.ts
+++ app/controller/account.ts
@@ -1,34 +1,42 @@
 import { Controller } from 'egg';
-import { parseNumber } from 'libphonenumber-js';
+// import { parseNumber } from 'libphonenumber-js';
 
 export default class AccountController extends Controller {
   public async signIn() {
-    const {  ctx } = this;
+    const { ctx } = this;
 
-    const { phone, code } = ctx.request.body;
-
-    if (!phone) {
-      ctx.error('ERR_NULL_PHONE_NO');
-    }
-
-    const { country } = parseNumber(phone);
-
-    if (!country) {
-      ctx.error('ERR_FORMAT_PHONE_NO');
-    }
-
-    if (country !== 'CN') {
-      ctx.error('NOT_SUPPORT_PHONE_NO');
-    }
+    const { phone, password, code } = ctx.request.body;
+    ctx.service.validate.phone(phone);
 
     const verifyCode = await ctx.service.cache.get(phone + '-verify');
+
     if (!verifyCode || code !== verifyCode) {
       ctx.error('ERR_VERIFY_CODE');
     }
 
-    ctx.body = {
+    const create = {
       phone,
-      verifyCode,
+      password,
+      code,
     };
+
+    let ret;
+
+    try {
+      ret = await ctx.model.User.create(create);
+    } catch (e) {
+      console.log(e);
+    }
+    // const ret = 
+
+    ctx.body = {
+      id: ret.id,
+    };
+  }
+
+  public async login() {
+    const { ctx } = this;
+
+    ctx.body = {};
   }
 }
--- app/router.ts
+++ app/router.ts
@@ -5,6 +5,7 @@ export default (app: Application) => {
 
   router.get('/', controller.home.index);
   router.post('/v1/sign-in', controller.account.signIn);
+  router.post('/v1/login', controller.account.login);
   router.post('/v1/verify-code/sms-code', controller.verify.smsCode);
   router.post('/v1/verify-code/email-code', controller.verify.emailCode);
   // router.post('/v1/api/auth/user-current', controller.auth.userCurrent)
