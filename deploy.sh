b6c78349a3e5e8b3f2d4c843878199a3dfcdd772
xank <xank@qq.com>
2018-06-01 18:41:36 +0800

    22001

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ docker stop user-center-service \
 ; cd /app/user-center-service \
 && git pull \
 && docker build -t user-center-service . \
-&& docker run -e TZ="Asia/Shanghai" -d -p 20001:7001 --name user-center-service \
+&& docker run -e TZ="Asia/Shanghai" -d -p 22001:7001 --name user-center-service \
 --mount type=bind,source=/app/config/user-center-service,target=/app/config \
 user-center-service
